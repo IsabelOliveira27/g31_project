cb2b3ea5ba8487a7ae59f83b5797f26fe460a575
ConstaFig <up202506674@up.pt>
2026-04-26 19:39:10 +0100

    Update maintenance_event.py

--- classes/maintenance_event.py
+++ classes/maintenance_event.py
@@ -13,63 +13,63 @@ class Maintenance_event (Gclass):
     pos = 0
     sortkey =''
 
-att = ["_id","_equipment_id", "_maintenance_type_id" , "_Maintenance_Date", "_extra_info"]
+    att = ["_id","_equipment_id", "_maintenance_type_id" , "_Maintenance_Date", "_extra_info"]
 
-header = "Maintenance_event"
+    header = "Maintenance_event"
 
-des = ["id","equipment_id", "maintenance_type_id" , "Maintenance_Date", "extra_info"]
+    des = ["id","equipment_id", "maintenance_type_id" , "Maintenance_Date", "extra_info"]
 
-def __init__(self,id, equipment_id, maintenance_type_id, Maintenance_Date, extra_info):
-    super().__init__()
+    def __init__(self,id, equipment_id, maintenance_type_id, Maintenance_Date, extra_info):
+        super().__init__()
 
-    id = Maintenance_event.get_id(id)
-    self._id = id
-    self._maintenance_type_id = maintenance_type_id
-    self._equipment_id = equipment_id
-    self._extra_info = extra_info
-    self._Maintenance_Date = datetime.strptime(Maintenance_Date, "%d/%m/%Y"). date()
+        id = Maintenance_event.get_id(id)
+        self._id = id
+        self._maintenance_type_id = maintenance_type_id
+        self._equipment_id = equipment_id
+        self._extra_info = extra_info
+        self._Maintenance_Date = datetime.strptime(Maintenance_Date, "%d/%m/%Y"). date()
 
-    Maintenance_event.obj[id] = self
-
-    Maintenance_event.lst.append(id)
-
-@property
-def id(self):
-    return self._id
-
-@id.setter
-def id(self,id):
-    self._id = id
-
-@property
-def maintenance_type_id(self):
-    return self._maintenance_type_id
-
-@maintenance_type_id.setter
-def maintenance_type_id(self, maintenance_type_id):
-    self._maintenance_type_id = maintenance_type_id
+        Maintenance_event.obj[id] = self
     
-@property
-def equipment_id(self):
-    return self._equipment_id
+        Maintenance_event.lst.append(id)
 
-@equipment_id.setter
-def equipment_id(self, equipment_id):
-    self._equipment_id= equipment_id
-
-
-@property
-def Maintenance_Date(self):
-    return self._maintenance_date
-
-@Maintenance_Date. setter
-def Maintenance_Date(self, Maintenance_Date):
-    self._Maintenance_Date = Maintenance_Date
-
-@property
-def extra_info(self):
-    return self._extra_info
-
-@extra_info.setter
-def extra_info(self, extra_info) :
-    self._extra_info = extra_info
+    @property
+    def id(self):
+        return self._id
+    
+    @id.setter
+    def id(self,id):
+        self._id = id
+    
+    @property
+    def maintenance_type_id(self):
+        return self._maintenance_type_id
+    
+    @maintenance_type_id.setter
+    def maintenance_type_id(self, maintenance_type_id):
+        self._maintenance_type_id = maintenance_type_id
+        
+    @property
+    def equipment_id(self):
+        return self._equipment_id
+    
+    @equipment_id.setter
+    def equipment_id(self, equipment_id):
+        self._equipment_id= equipment_id
+    
+    
+    @property
+    def Maintenance_Date(self):
+        return self._maintenance_date
+    
+    @Maintenance_Date. setter
+    def Maintenance_Date(self, Maintenance_Date):
+        self._Maintenance_Date = Maintenance_Date
+    
+    @property
+    def extra_info(self):
+        return self._extra_info
+    
+    @extra_info.setter
+    def extra_info(self, extra_info) :
+        self._extra_info = extra_info
