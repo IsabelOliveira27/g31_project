db2b66f9ecc2778179a85f68125a42348b990ef1
up202508222@up.pt <up202508222@up.pt>
2026-06-08 00:27:03 +0100

    Update equipment_operator.py

--- classes/equipment_operator.py
+++ classes/equipment_operator.py
@@ -5,10 +5,9 @@ Created on Mon Apr 20 13:56:42 2026
 @author: isabel
 """
 from classes.gclass import Gclass
-from datetime import datetime
+import datetime
 
-
-class Equipment_Operator(Gclass):
+class Equipment_operator(Gclass):
     obj = dict()
     lst = list()
     pos = 0
@@ -18,75 +17,69 @@ class Equipment_Operator(Gclass):
 
     def __init__(self, id, equipment_id, operator_id, utilization_date, cost):
         super().__init__()
-        
-        novo_id = Equipment_Operator.get_id(id)
+        novo_id = Equipment_operator.get_id(id)
         self._id = novo_id
         self._equipment_id = int(equipment_id)
         self._operator_id = int(operator_id)
-        self._utilization_date = utilization_date 
         self._cost = float(cost)
         
+        if isinstance(utilization_date, (datetime.date, datetime.datetime)):
+            self._utilization_date = utilization_date
+        else:
+            try:
+                self._utilization_date = datetime.datetime.strptime(str(utilization_date).strip(), "%d/%m/%Y").date()
+            except ValueError:
+                self._utilization_date = datetime.datetime.strptime(str(utilization_date).strip(), "%d/%m/%Y").date()
 
-        Equipment_Operator.obj[novo_id] = self
-        Equipment_Operator.lst.append(novo_id)
+        Equipment_operator.obj[novo_id] = self
+        if novo_id not in Equipment_operator.lst: Equipment_operator.lst.append(novo_id)
 
     @property
-    def id(self):
+    def id(self): 
         return self._id
-
+    
     @id.setter
-    def id(self, value):
+    def id(self, value): 
         self._id = value
         
     @property
-    def operator_id(self):
+    def operator_id(self): 
         return self._operator_id
     
+    
     @operator_id.setter
-    def operator_id(self, operator_id):
-        self._operator_id = operator_id
+    def operator_id(self, operator_id): 
+        self._operator_id = int(operator_id)
         
     @property    
-    def equipment_id(self):
+    def equipment_id(self): 
         return self._equipment_id
     
     @equipment_id.setter
-    def equipment_id(self, equipment_id):
-        self._equipment_id = equipment_id
+    def equipment_id(self, equipment_id): 
+        self._equipment_id = int(equipment_id)
 
     @property
     def utilization_date(self):
-        if isinstance(self._utilization_date, str):
-            return datetime.strptime(self._utilization_date, "%d/%m/%Y")
-        return self._utilization_date
-
+        return self._utilization_date.strftime('%d/%m/%Y')
+    
     @utilization_date.setter
     def utilization_date(self, value):
-        self._utilization_date = value
+        if isinstance(value, (datetime.date, datetime.datetime)):
+            self._utilization_date = value
+        else:
+            try:
+                self._utilization_date = datetime.datetime.strptime(str(value).strip(), "%d/%m/%Y").date()
+            except ValueError:
+                self._utilization_date = datetime.datetime.strptime(str(value).strip(), "%d/%m/%Y").date()
 
     @property
-    def cost(self):
-        return self._cost
-
+    def cost(self): return self._cost
     @cost.setter
-    def cost(self, value):
-        self._cost = float(value)
-
-    @property
-    def get_year(self):
-        return str(self._utilization_date).split('/')[-1]
-
-    @property
-    def is_weekend_operation(self):
-        dt = self.utilization_date
-        return dt.weekday() >= 5
+    def cost(self, value): self._cost = float(value)
 
     @property
     def get_efficiency_score(self):
-        if self._cost == 0:
-            return "N/A"
+        if self._cost == 0: return "N/A"
         return round(1000 / self._cost, 2)
 
-    def __str__(self):
-        return f"Log:{self._id}|Op:{self._operator_id}|Eq:{self._equipment_id}|Date:{self._utilization_date}|Cost:{self._cost}"
-    
