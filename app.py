cdc9fad6ba1beb77c4f16963b3cd7ea4a92324d1
up202506533 <up202506533@up.pt>
2026-06-07 10:44:39 +0100

    Update app.py

--- app.py
+++ app.py
@@ -592,12 +592,6 @@ def subform(cname):
 def userlogin():
     return apps_userlogin()
 
-if __name__ == '__main__':
-    app.run(debug=True, use_reloader=False)
-
-
-
-
 
 if __name__ == '__main__':
     app.run(debug=True, use_reloader=False)
