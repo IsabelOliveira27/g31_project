f5aec87f3f562fb623a8e990bf514a65a3fad55c
up202508222@up.pt <up202508222@up.pt>
2026-06-07 16:12:22 +0100

    Update main.js

--- static/js/main.js
+++ static/js/main.js
@@ -37,7 +37,7 @@ function initAutocomplete(inputId, boxId, selectId, apiUrl) {
                             div.style.display = 'flex';
                             div.style.justifyContent = 'space-between';
                             
-                            div.innerHTML = '<span>🔍 ' + item.texto + '</span><small style="color:#94a3b8">' + item.qtd + ' registo(s)</small>';
+                            div.innerHTML = '<span>üîç ' + item.texto + '</span><small style="color:#94a3b8">' + item.qtd + ' registo(s)</small>';
                             
                             div.addEventListener('click', function() {
                                 searchInput.value = item.texto;
@@ -70,7 +70,7 @@ function initAutocomplete(inputId, boxId, selectId, apiUrl) {
             if (attribute === 'id' || attribute.endsWith('_id')) {
                 if (text.length > 0 && validSuggestions.length > 0 && !validSuggestions.includes(text.toLowerCase())) {
                     e.preventDefault();
-                    alert("O " + labelText + " '" + text + "' não existe no sistema!");
+                    alert("O " + labelText + " '" + text + "' n√£o existe no sistema!");
                 }
             }
         });
@@ -103,7 +103,7 @@ document.addEventListener("DOMContentLoaded", function () {
                 const stringId = parseInt(parts[0], 10);
                 if (!isNaN(stringId) && stringId < 0) {
                     e.preventDefault();
-                    alert("O ID '" + parts[0] + "' não é válido ou não pertence a um grupo existente!");
+                    alert("O ID '" + parts[0] + "' n√£o √© v√°lido ou n√£o pertence a um grupo existente!");
                     return;
                 }
             }
@@ -137,7 +137,7 @@ document.addEventListener("DOMContentLoaded", function () {
                     }
                     
                     e.preventDefault();
-                    alert("O " + labelText + " '" + input.value + "' não pertence a um grupo existente!");
+                    alert("O " + labelText + " '" + input.value + "' n√£o pertence a um grupo existente!");
                     return;
                 }
             }
